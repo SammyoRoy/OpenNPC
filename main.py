049ccd45a362a3afc6f0115278ab65562b6df902
SammyoRoy <sammyo.roychowdhury@gmail.com>
2025-02-06 19:49:51 -0800

    Adds basic I/O

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    # Prompt the user for input
+    user_input = input("Hi what's your name:\n")
+    # Output "Hello" (you could also use the input if desired)
+    print("Hello "+user_input)
+
+if __name__ == "__main__":
+    main()
